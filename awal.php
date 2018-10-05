e4cd2b4ebb4a36097d46ccdea62fd2e4673c7db6
rizkhita <kikirizkhita@gmail.com>
2018-10-06 06:06:57 +0700

    kripto

---
+++ awal.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+	<title>FORM UNTUK ENKRIPSI</title>
+</head>
+<body>
+	<form action="enkcaesar.php" method="get">
+	Plainteks    : <input type="text" name="kata"> <br>
+	Key   : <input type="text" name="key" maxlength="2"> <br>
+	<input type="submit" value="kirim">
+	<input type="reset" value="ulangi">
+	</form>
+</body>
+</html>
